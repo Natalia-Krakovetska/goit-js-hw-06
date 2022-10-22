02bd219ac4f87cb8d7330413ed74c7677cca0858
Natalia <natalakrakoveckaya@gmail.com>
2022-10-22 19:51:02 +0200

    task2_Done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listIngredientsEl = document.querySelector('#ingredients');
+const elementsIngredients = [];
+for (let i = 0; i < ingredients.length; i+=1) {
+  const element = ingredients[i];
+  
+    const itemOfingredientsEl = document.createElement('li');
+    itemOfingredientsEl.classList = 'item';
+    itemOfingredientsEl.textContent = element; 
+  
+    elementsIngredients.push(itemOfingredientsEl);
+}
+listIngredientsEl.append(...elementsIngredients);
+
+
