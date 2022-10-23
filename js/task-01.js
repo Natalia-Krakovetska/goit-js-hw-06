9aaba32b44afa0907ca5d0d68074394e3ede3da4
Natalia <natalakrakoveckaya@gmail.com>
2022-10-23 15:14:04 +0200

    update task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,26 +1,29 @@
-const itemsEl = document.querySelectorAll('.item');
-console.log("Number of categories :", itemsEl.length);
+const listEl = document.querySelector('#categories');
+console.log("Number of categories :", listEl.children.length);
 
+const titlesOfcategoriesEl = document.querySelectorAll('.item');
+for(const title of titlesOfcategoriesEl){
+console.log('Category :', title.firstElementChild.textContent);
+console.log('Elements :', title.lastElementChild.children.length);
+}
 
-const firstItemEl = itemsEl[0];
-const firstTitleEl = firstItemEl.firstElementChild;
-console.log('Category :', firstTitleEl.textContent);
-const firstListOfGoodsEl = firstItemEl.lastElementChild;
-const firstItemsOfGoodsEl = firstListOfGoodsEl.children;
-console.log('Elements :', firstItemsOfGoodsEl.length);
+// const firstItemEl = itemsEl[0];
+// const firstTitleEl = firstItemEl.firstElementChild;
+// console.log('Category :', firstTitleEl.textContent);
+// const firstListOfGoodsEl = firstItemEl.lastElementChild;
+// const firstItemsOfGoodsEl = firstListOfGoodsEl.children;
+// console.log('Elements :', firstItemsOfGoodsEl.length);
 
-const secondItemEl =  itemsEl[1];
-const secondTitleEl = secondItemEl.firstElementChild;
-console.log('Category :', secondTitleEl.textContent);
-const secondListOfGoodsEl = secondItemEl.lastElementChild;
-const secondItemsOfGoodsEl = secondListOfGoodsEl.children;
-console.log('Elements :', secondItemsOfGoodsEl.length);
+// const secondItemEl =  itemsEl[1];
+// const secondTitleEl = secondItemEl.firstElementChild;
+// console.log('Category :', secondTitleEl.textContent);
+// const secondListOfGoodsEl = secondItemEl.lastElementChild;
+// const secondItemsOfGoodsEl = secondListOfGoodsEl.children;
+// console.log('Elements :', secondItemsOfGoodsEl.length);
 
-const thirdItemEl =  itemsEl[2];
-const thirdTitleEl = thirdItemEl.firstElementChild;
-console.log('Category :', thirdTitleEl.textContent);
-const thirdListOfGoodsEl = thirdItemEl.lastElementChild;
-const thirdItemsOfGoodsEl = thirdListOfGoodsEl.children;
-console.log('Elements :', thirdItemsOfGoodsEl.length);
-
-
+// const thirdItemEl =  itemsEl[2];
+// const thirdTitleEl = thirdItemEl.firstElementChild;
+// console.log('Category :', thirdTitleEl.textContent);
+// const thirdListOfGoodsEl = thirdItemEl.lastElementChild;
+// const thirdItemsOfGoodsEl = thirdListOfGoodsEl.children;
+// console.log('Elements :', thirdItemsOfGoodsEl.length);
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,34 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+// const option = images[0];
+  const listOfImagesEl = document.querySelector('.gallery');
+console.log(listOfImagesEl);
+const elements = images.map(({url, alt}) =>{
+  const itemOfList = listOfImagesEl.insertAdjacentHTML("afterbegin",`<li><img src = `${url}`, alt = `${alt}`></li>`);
+
+  return itemOfList;
+
+})
+
+// const makeListOfImages = images => {
+//   return images.map(option =>{
+//     const itemOfList = listOfImagesEl.insertAdjacentHTML("afterbegin",'<li></li>');
+//     const imageInItem = document.createElement("<img>")
+//     imageInItem.url = option.url;
+//     imageInItem.alt = option.alt;
+
+//     return itemOfList;
+// })
+// }
+// const elements = makeListOfImages(images);
+
+// const listEl=document.querySelector(".gallery")
+// console.log(listEl);
+// const markup = images.map(({
+//   url,
+//   alt
+// }) =>
+//   `<li>
+//   <img src="${url}" alt="${alt}" class="image-gallery">
+//   </li>`).join('');
