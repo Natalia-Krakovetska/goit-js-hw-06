10eb2e6b8d02b8bf64d1fc29074ba8a808798aba
Natalia <natalakrakoveckaya@gmail.com>
2022-10-26 13:52:47 +0200

    task9_done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+let bodyEl= document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+let textEl = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click',changeColorBody)
+
+function changeColorBody(){
+  bodyEl.style.backgroundColor =  getRandomHexColor();
+  textEl.textContent = bodyEl.style.backgroundColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
