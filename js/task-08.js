9fea90db1b9616d8a67cf654d3c78f01d1ba194e
Natalia <natalakrakoveckaya@gmail.com>
2022-10-26 12:56:33 +0200

    task8_done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+      elements: { email, password }
+    } = event.currentTarget;
+  
+    if (email.value === "" || password.value === "") {
+      alert("Все поля должны быть заполнены!");
+    }
+  
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+  }
