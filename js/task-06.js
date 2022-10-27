551692ccedf19cbe71ac781352b4ecdd657949e3
Natalia <natalakrakoveckaya@gmail.com>
2022-10-27 17:34:47 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,18 +3,18 @@ const inputTextEl = document.querySelector('#validation-input');
 inputTextEl.addEventListener('blur', onInputBlur)
 
 function onInputBlur(){
-    if(inputTextEl.value.length === 0)
-    {
-        inputTextEl.getAttribute('data-length');
-    }
-    else  if(inputTextEl.value.length < Number(inputTextEl.getAttribute('data-length')))
-    {
-        inputTextEl.classList.add('invalid');
-        inputTextEl.classList.remove('valid');
-    } 
-    else   
+
+    if(inputTextEl.value.length === Number(inputTextEl.getAttribute('data-length')))  
     {
         inputTextEl.classList.add('valid');
         inputTextEl.classList.remove('invalid');
+    }else if(inputTextEl.value.length < Number(inputTextEl.getAttribute('data-length')) && (inputTextEl.value.length > 0))
+    {
+        inputTextEl.classList.add('invalid');
+        inputTextEl.classList.remove('valid');
+    } else {
+        inputTextEl.classList.remove('invalid');
+        inputTextEl.classList.remove('valid');
     }
+
 }
